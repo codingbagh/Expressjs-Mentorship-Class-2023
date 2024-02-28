8744ed60a9e96c51ba04f12f7caece4275bba60c
codingba <codingbakelvin@gmail.com>
2024-02-28 22:31:52 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import productRouter from './routers/productRoute.js';
 import "./model/db.js";
+import userRouter from './routers/userRegistration.js';
+import cors from 'cors'
+
 
 const server = express();
 server.use(express.json());
+server.use(cors())
 
 server.use('/api/products',productRouter)
+server.use('/api/users',userRouter)
 
 const port = 8080;
 
