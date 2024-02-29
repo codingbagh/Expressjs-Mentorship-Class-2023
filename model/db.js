724c8ea930e38609fd54a7b99640b1a28e605765
codingba <codingbakelvin@gmail.com>
2024-02-29 22:36:58 +0000

    Update db.js

--- model/db.js
+++ model/db.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
-
+import dotenv from 'dotenv';
+dotenv.config();
+const connectionString = process.env.MONGO_DB_CONNECTION_STRING
 try{
-    mongoose.connect('mongodb+srv://coding:vLOTsdR38y8o4Ovu@cluster0.z3gc71x.mongodb.net/codingstore');
+    mongoose.connect(connectionString);
     console.log("Connected to MongoDB");
 
 }catch(err){
